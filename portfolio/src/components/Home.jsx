95d945fd6315e223a5a97a114e9142816d2e368d
Anne Bellefleur <annebellefleur@Annes-MacBook-Air.local>
2020-06-23 19:43:41 -0400

    created footer component, added styling to footer and header

--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -3,17 +3,20 @@ import About from './about/about'
 import Projects from './projects/projects'
 import Resume from './resume/resume'
 import Contact from './contact/contact'
+import Footer from './footer/footer'
+
+import './home.css'
 
 export default class Home extends Component {
     render() {
         return (
             <div>
-                <header>This is the header</header>
+                <header className='header'>This is the header</header>
                 <About />
                 <Projects />
                 <Resume />
                 <Contact />
-                <footer>This is the footer</footer>
+                <Footer />
             </div>
         )
     }
---
+++ portfolio/src/components/footer/footer.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+import './footer.css'
+
+export default class footer extends Component {
+    render() {
+        return (
+            <div>
+                <footer className='footer'>This is the footer</footer>
+            </div>
+        )
+    }
+}
