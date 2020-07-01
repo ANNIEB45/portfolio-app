e729f3777ae2edb8b5d236db04132c7d341d3951
Anne Bellefleur <annebellefleur@Annes-MacBook-Air.local>
2020-07-01 19:40:28 -0400

    added axios and router package

--- portfolio/src/components/contact/contact.jsx
+++ portfolio/src/components/contact/contact.jsx
@@ -3,12 +3,47 @@ import React, { Component } from 'react'
 import './contact.css'
 
 export default class contact extends Component {
+    state = {
+        name: '',
+        email: '',
+        message: ''
+    }
+
+    // componentDidMount(){
+        
+    // }
+
+    // handeleOnSubmit = () => {
+        
+    // }
+
     render() {
         return (
             <div className='contact'>
-                <h1>Contact</h1>
-                <form />
-                
+                <div>Contact Me</div>
+                <form>
+                    <label>Name
+                     <input
+                            type='text'
+                            name='name'
+                        />
+                    </label>
+                    <label>Email
+                     <input
+                            type='text'
+                            name='email'
+                        />
+                    </label>
+                    <label>Message
+                     <input
+                            type='text'
+                            name='message'
+                        />
+                    </label>
+                </form>
+
+
+
             </div>
         )
     }
