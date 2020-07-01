8be3943763b3aa5fafaffd1ce6be5b9986ec85a0
Anne Bellefleur <annebellefleur@Annes-MacBook-Air.local>
2020-07-01 18:12:49 -0400

    added links to social

--- portfolio/src/components/footer/footer.jsx
+++ portfolio/src/components/footer/footer.jsx
@@ -10,12 +10,14 @@ export default class footer extends Component {
             <footer className='footer'>
                 <div className='social-links'>
                     <div className='linkedin-wrapper'>
-                        <a href='https://www.linkedin.com/in/annie-t-bellefleur-04a696a7/' target='_blank' rel="noopener noreferrer">LinkedIn
-                          
-                        </a>
-                        </div>
-                    <div className='twitter-wrapper'>Twitter</div>
-                    <div className='github-wrapper'>GitHub</div>
+                        <a href='https://www.linkedin.com/in/annie-t-bellefleur-04a696a7/' target='_blank' rel="noopener noreferrer">LinkedIn</a>
+                    </div>
+                    <div className='twitter-wrapper'>
+                        <a href='https://twitter.com/AfiaTheLegend' target='_blank' rel="noopener noreferrer">Twitter</a>
+                    </div>
+                    <div className='github-wrapper'>
+                        <a href='https://github.com/ANNIEB45' target='_blank' rel="noopener noreferrer">GitHub</a>
+                    </div>
                 </div>
                 <div className='resources-container'>
                     <div>Blog</div>
