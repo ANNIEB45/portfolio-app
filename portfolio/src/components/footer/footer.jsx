ab7671eb0995e49fdb17f79cf079d71d1cad0d0d
Anne Bellefleur <annebellefleur@Annes-MacBook-Air.local>
2020-07-01 18:53:41 -0400

    color, margin, width, height for icons

--- portfolio/src/components/footer/footer.jsx
+++ portfolio/src/components/footer/footer.jsx
@@ -1,5 +1,9 @@
 import React, { Component } from 'react'
 
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import GitHubIcon from '@material-ui/icons/GitHub';
+
 
 import './footer.css'
 
@@ -10,13 +14,19 @@ export default class footer extends Component {
             <footer className='footer'>
                 <div className='social-links'>
                     <div className='linkedin-wrapper'>
-                        <a href='https://www.linkedin.com/in/annie-t-bellefleur-04a696a7/' target='_blank' rel="noopener noreferrer">LinkedIn</a>
+                        <a href='https://www.linkedin.com/in/annie-t-bellefleur-04a696a7/' target='_blank' rel="noopener noreferrer">
+                            <LinkedInIcon id='footer-linkedin' />
+                        </a>
                     </div>
                     <div className='twitter-wrapper'>
-                        <a href='https://twitter.com/AfiaTheLegend' target='_blank' rel="noopener noreferrer">Twitter</a>
+                        <a href='https://twitter.com/AfiaTheLegend' target='_blank' rel="noopener noreferrer">
+                            <TwitterIcon id='footer-twitter' />
+                        </a>
                     </div>
                     <div className='github-wrapper'>
-                        <a href='https://github.com/ANNIEB45' target='_blank' rel="noopener noreferrer">GitHub</a>
+                        <a href='https://github.com/ANNIEB45' target='_blank' rel="noopener noreferrer">
+                            <GitHubIcon id='footer-github' />
+                        </a>
                     </div>
                 </div>
                 <div className='resources-container'>
