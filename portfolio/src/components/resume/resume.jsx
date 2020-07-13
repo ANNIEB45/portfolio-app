3f5d1cdad1b89963610701ad4ddbbf011df85c32
Anne Bellefleur <annebellefleur@Annes-MacBook-Air.local>
2020-07-12 22:59:12 -0400

    changed height to auto

--- portfolio/src/components/resume/resume.jsx
+++ portfolio/src/components/resume/resume.jsx
@@ -10,8 +10,22 @@ export default class resume extends Component {
                     <div>
                         <h3>Relevant Experience</h3>
                     </div>
-                    <div>Skills</div>
-                    <div>Work Experience</div>
+                    <div>
+                        <ul>Skills
+                        <li>JavaScript</li>
+                            <li>Ruby on Rails</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h3>Work Experience</h3>
+                        <ul>
+                            <div>
+                                <div>Grady Hospital</div>
+                                <div>Atlanta, GA</div>
+                                <div>Description about job, accomplishements, etc</div>
+                            </div>
+                        </ul>
+                    </div>
                 </div>
             </div>
         )
