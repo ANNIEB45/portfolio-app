ea9e40e598fe8f244bcecbfe34383057f053571e
Anne Bellefleur <annebellefleur@Annes-MacBook-Air.local>
2020-07-14 15:58:32 -0400

    added intro component

--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -5,6 +5,7 @@ import Projects from './projects/projects'
 import Resume from './resume/resume'
 import Contact from './contact/contact'
 import Footer from './footer/footer'
+import Intro from "./IntroPage/intro"
 
 import './home.css'
 
@@ -12,12 +13,15 @@ export default class Home extends Component {
     render() {
         return (
             <div>
+
                 <Header />
+                <Intro />
                 <About />
                 <Resume />
                 <Projects />
                 <Contact />
                 <Footer />
+
             </div>
         )
     }
