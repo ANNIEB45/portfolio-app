8f3220b340366220ddd964d8615e875c833d06c8
Anne Bellefleur <annebellefleur@Annes-MacBook-Air.local>
2020-07-14 17:13:26 -0400

    updated styling

--- portfolio/src/components/resume/resume.jsx
+++ portfolio/src/components/resume/resume.jsx
@@ -7,23 +7,32 @@ export default class resume extends Component {
         return (
             <div className='resume'>
                 <div>
-                    <div>
-                        <h3>Relevant Experience</h3>
+                    <div className="section1">
+                        <div>
+                            <h3>Experience</h3>
+                            <ul>
+                                <div>
+                                    <div>Grady Hospital</div>
+                                    <div>Atlanta, GA</div>
+                                    <div>Description about job, accomplishements, etc</div>
+                                </div>
+                            </ul>
+                        </div>
+                        <div>
+                            <h3>Education</h3>
+                            <ul>
+                                <div>
+                                    <div>General Assembly</div>
+                                    <div>Atlanta, GA</div>
+                                    <div>Description about job, accomplishements, etc</div>
+                                </div>
+                            </ul>
+                        </div>
                     </div>
                     <div>
                         <ul>Skills
                         <li>JavaScript</li>
                             <li>Ruby on Rails</li>
-                        </ul>
-                    </div>
-                    <div>
-                        <h3>Work Experience</h3>
-                        <ul>
-                            <div>
-                                <div>Grady Hospital</div>
-                                <div>Atlanta, GA</div>
-                                <div>Description about job, accomplishements, etc</div>
-                            </div>
                         </ul>
                     </div>
                 </div>
