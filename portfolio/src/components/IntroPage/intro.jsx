ef2261da2e674c43121c26b55b947e029ec3e54f
Anne Bellefleur <annebellefleur@Annes-MacBook-Air.local>
2020-07-15 22:03:53 -0400

    added material-ui package and timeline API

--- portfolio/src/components/IntroPage/intro.jsx
+++ portfolio/src/components/IntroPage/intro.jsx
@@ -10,8 +10,8 @@ export default class intro extends Component {
 
                     <h1>Hi...
                     <br />I'm <span>Annie</span></h1>
-                    {/* <h2>Yes, I am ok...</h2> */}
-                    <p>Software Developer, food enthusiast, passionate smiler</p>
+                    {/* <h2>Yes, I am ok...</h2> */ }
+                    <p>Software Developer, Food Enthusiast, Committed Smiler</p>
 
                 </div>
                 <div className="scrolling"></div>
--- portfolio/src/components/resume/resume.jsx
+++ portfolio/src/components/resume/resume.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Timeline, TimelineItem, TimelineConnector, TimelineSeparator, TimelineContent, TimelineDot } from '@material-ui/lab'
 
 import './resume.css'
 
@@ -6,36 +7,64 @@ export default class resume extends Component {
     render() {
         return (
             <div className='resume'>
-                <div>
-                    <div className="section1">
+                <div className="section1">
+                    <div>
+
+                        <h3>Experience</h3>
+
                         <div>
-                            <h3>Experience</h3>
-                            <ul>
-                                <div>
-                                    <div>Grady Hospital</div>
-                                    <div>Atlanta, GA</div>
-                                    <div>Description about job, accomplishements, etc</div>
-                                </div>
-                            </ul>
+                            <Timeline align='left'>
+                                <TimelineItem>
+                                    <TimelineSeparator>
+                                        <TimelineDot />
+                                        <TimelineConnector />
+                                    </TimelineSeparator>
+                                    <div>
+                                        <TimelineContent>Grady Hospital</TimelineContent>
+                                        <div>
+                                            <TimelineContent>Atlanta, GA </TimelineContent>
+                                            <TimelineContent>2012-2014</TimelineContent>
+                                        </div>
+                                        <TimelineContent>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </TimelineContent>
+                                    </div>
+                                </TimelineItem>
+                            </Timeline>
+
+
                         </div>
-                        <div>
-                            <h3>Education</h3>
-                            <ul>
-                                <div>
-                                    <div>General Assembly</div>
-                                    <div>Atlanta, GA</div>
-                                    <div>Description about job, accomplishements, etc</div>
-                                </div>
-                            </ul>
-                        </div>
+
                     </div>
                     <div>
-                        <ul>Skills
-                        <li>JavaScript</li>
-                            <li>Ruby on Rails</li>
-                        </ul>
+                        <h3>Education</h3>
+
+                        <div>
+                            <Timeline align='left'>
+                                <TimelineItem>
+                                    <TimelineSeparator>
+                                        <TimelineDot />
+                                        <TimelineConnector />
+                                    </TimelineSeparator>
+                                    <div>
+                                        <TimelineContent>Grady Hospital</TimelineContent>
+                                        <div>
+                                            <TimelineContent>Atlanta, GA </TimelineContent>
+                                            <TimelineContent>2012-2014</TimelineContent>
+                                        </div>
+                                        <TimelineContent>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </TimelineContent>
+                                    </div>
+                                </TimelineItem>
+                            </Timeline>
+                        </div>
+
                     </div>
                 </div>
+                <div className='section2'>
+                    <ul>Skills
+                        <li>JavaScript</li>
+                        <li>Ruby on Rails</li>
+                    </ul>
+                </div>
+
             </div>
         )
     }
