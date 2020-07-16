51278a0c3000840dc64bf770ffa654631c8eea7d
Anne Bellefleur <annebellefleur@Annes-MacBook-Air.local>
2020-07-16 00:38:02 -0400

    updated

--- portfolio/src/components/about/about.jsx
+++ portfolio/src/components/about/about.jsx
@@ -7,9 +7,9 @@ export default class about extends Component {
         return (
             <div className='about'>
                 <div>
-                    
                     <img src="https://cdn2.lamag.com/wp-content/uploads/sites/6/2017/07/issa.jpg" alt="placeholder of Issa Rae" />
                 </div>
+
                 <div className='about-paragraph'>
                     <h4>Software Engineer</h4>
                     <p>Since middle I've toyed with programming. First in (some program) and now in Software Development. For the past two years I've learned...</p>
