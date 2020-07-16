9ba118775789fb4ac436dc189c487e8163a5a917
Anne Bellefleur <annebellefleur@Annes-MacBook-Air.local>
2020-07-15 22:17:10 -0400

    added info to education timeline

--- portfolio/src/components/resume/education.jsx
+++ portfolio/src/components/resume/education.jsx
@@ -12,10 +12,25 @@ export default class education extends Component {
                             <TimelineConnector />
                         </TimelineSeparator>
                         <div>
-                            <TimelineContent>Grady Hospital</TimelineContent>
+                            <TimelineContent>General Assembly</TimelineContent>
                             <div>
                                 <TimelineContent>Atlanta, GA </TimelineContent>
-                                <TimelineContent>2012-2014</TimelineContent>
+                                <TimelineContent>March 2020-June 2020</TimelineContent>
+                            </div>
+                            <TimelineContent>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </TimelineContent>
+                        </div>
+                    </TimelineItem>
+
+                    <TimelineItem>
+                        <TimelineSeparator>
+                            <TimelineDot />
+                            <TimelineConnector />
+                        </TimelineSeparator>
+                        <div>
+                            <TimelineContent>Corinthians College</TimelineContent>
+                            <div>
+                                <TimelineContent>Atlanta, GA </TimelineContent>
+                                <TimelineContent>August 2012- September 2014</TimelineContent>
                             </div>
                             <TimelineContent>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </TimelineContent>
                         </div>
