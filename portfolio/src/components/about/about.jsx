72e3d1432b2200ff84c542e4b3dd0fa8a3a0fe6d
ANNIEB45 <ANNIEBELLEFLEUR@GMAIL.com>
2020-07-26 18:30:53 -0400

    updated styling, added social icons

--- portfolio/src/components/about/about.jsx
+++ portfolio/src/components/about/about.jsx
@@ -1,20 +1,40 @@
 import React, { Component } from 'react'
 
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import GitHubIcon from '@material-ui/icons/GitHub';
+
 import './about.css'
 
 export default class about extends Component {
     render() {
         return (
             <div className='about'>
-                <div>
-                    <img src="https://cdn2.lamag.com/wp-content/uploads/sites/6/2017/07/issa.jpg" alt="placeholder of Issa Rae" />
+                <div className='about-container'>
+                    <div className='image-container'>
+                        <img src="https://cdn2.lamag.com/wp-content/uploads/sites/6/2017/07/issa.jpg" alt="placeholder of Issa Rae" />
+                        
+                        <div className="social-icon">
+                            <a href='https://www.linkedin.com/in/annie-t-bellefleur-04a696a7/' target='_blank' rel="noopener noreferrer">
+                                <LinkedInIcon id='footer-linkedin' />
+                            </a>
+                            <a href='https://twitter.com/AfiaTheLegend' target='_blank' rel="noopener noreferrer">
+                                <TwitterIcon id='footer-twitter' />
+                            </a>
+                            <a href='https://github.com/ANNIEB45' target='_blank' rel="noopener noreferrer">
+                                <GitHubIcon id='footer-github' />
+                            </a>
+                        </div>
+                    </div>
+
+                    <div className='about-paragraph'>
+                        <h2>About Me</h2>
+                        <h4>I am Software Engineer</h4>
+                        <p>Since middle I've toyed with programming. First in (some program) and now in Software Development. For the past two years I've learned...</p>
+                        <p>I am excited to begin my journey as a Software Engineer.</p>
+                    </div>
                 </div>
 
-                <div className='about-paragraph'>
-                    <h4>Software Engineer</h4>
-                    <p>Since middle I've toyed with programming. First in (some program) and now in Software Development. For the past two years I've learned...</p>
-                </div>
-                
             </div>
         )
     }
