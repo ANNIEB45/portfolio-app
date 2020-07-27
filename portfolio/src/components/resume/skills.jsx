b348a0bddb158d12aa3da47dc5bf254a348f6e0f
ANNIEB45 <ANNIEBELLEFLEUR@GMAIL.com>
2020-07-26 20:38:12 -0400

    updated styling

--- portfolio/src/components/resume/skills.jsx
+++ portfolio/src/components/resume/skills.jsx
@@ -6,23 +6,25 @@ export default class skills extends Component {
         return (
             <div className="skills">
                 <h3>Skills</h3>
-                    <div className="skills-icon">
-                        <img src='/html5.png' alt=""/>
-                    <img src="/javascriprt.png" alt="JavaScript log" />
-                    <img src="/sql.png" alt="SQL log" />
-                    {/* Icons made by <a href="https://www.flaticon.com/authors/pixel-perfect" title="Pixel perfect">Pixel perfect</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a> */}
-                    <img src="/python.png" alt="" />
-                    <img src="/rubyonrails.png" alt="" />
-                    <img src="/css.png" alt="" />
-                    <img src="/ruby.png" alt="" />
-                    <img src="/docker.png" alt="" />
-                    <img src="nodejs.png" alt="" />
-                    <img src="trello.png" alt="" />
-                    <img src="/mongodb.png" alt="" />
-                    <img src="/mysql.png" alt="" />
-                    <img src="java.png" alt="" />
-                    <img src="react.png" alt=""/>
-                    </div>
+                <div className="skills-icon">
+                    <img src='/html5.png' alt="HTML logo" className="image-overlay" />
+                    {/* <div class="overlay-text">My Name is John</div> */}
+
+                    <img src="/javascriprt.png" alt="JavaScript logo" />
+                    <img src="/sql.png" alt="SQL logo" />
+                    {/* Icons made by <a href="https://www.flaticon.com/authors/pixel-perfect" title="Pixel perfect">Pixel perfect</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a> */ }
+                    <img src="/python.png" alt="Python logo" />
+                    <img src="/rubyonrails.png" alt="Ruby on Rails logo" />
+                    <img src="/css.png" alt="CSS logo" />
+                    <img src="/ruby.png" alt="Ruby logo" />
+                    <img src="/docker.png" alt="Docker logo" />
+                    <img src="nodejs.png" alt="Node JS logo" />
+                    <img src="trello.png" alt="Trello logo" />
+                    <img src="/mysql.png" alt="MySQL logo" />
+                    <img src="java.png" alt="Java logo" />
+                    <img src="react.png" alt="React logo" />
+                </div>
+                {/* figure out how to add tooltip */}
             </div>
         )
     }
