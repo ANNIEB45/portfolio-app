2f427d9496e8ff34d1f32d71f7e75d6293a5d834
ANNIEB45 <ANNIEBELLEFLEUR@GMAIL.com>
2020-07-26 20:03:37 -0400

    added skills icons and styled

--- portfolio/src/components/resume/skills.jsx
+++ portfolio/src/components/resume/skills.jsx
@@ -1,14 +1,28 @@
 import React, { Component } from 'react'
 
+
 export default class skills extends Component {
     render() {
         return (
-            <div>
+            <div className="skills">
                 <h3>Skills</h3>
-                    <ul>
-                        <li>JavaScript</li>
-                        <li>Ruby on Rails</li>
-                    </ul>
+                    <div className="skills-icon">
+                        <img src='/html5.png' alt=""/>
+                    <img src="/javascriprt.png" alt="JavaScript log" />
+                    <img src="/sql.png" alt="SQL log" />
+                    {/* Icons made by <a href="https://www.flaticon.com/authors/pixel-perfect" title="Pixel perfect">Pixel perfect</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a> */}
+                    <img src="/python.png" alt="" />
+                    <img src="/rubyonrails.png" alt="" />
+                    <img src="/css.png" alt="" />
+                    <img src="/ruby.png" alt="" />
+                    <img src="/docker.png" alt="" />
+                    <img src="nodejs.png" alt="" />
+                    <img src="trello.png" alt="" />
+                    <img src="/mongodb.png" alt="" />
+                    <img src="/mysql.png" alt="" />
+                    <img src="java.png" alt="" />
+                    <img src="react.png" alt=""/>
+                    </div>
             </div>
         )
     }
