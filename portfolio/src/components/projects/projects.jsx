fc45276fa63931e79605c4a811d7a2bd0868d882
ANNIEB45 <ANNIEBELLEFLEUR@GMAIL.com>
2020-07-26 20:56:30 -0400

    added styling to project component

--- portfolio/src/components/projects/projects.jsx
+++ portfolio/src/components/projects/projects.jsx
@@ -7,11 +7,19 @@ export default class projects extends Component {
         return (
             <div className='project'>
                 <h1>Projects</h1>
+                <div className="project-container">
+                    
 
-                <a href="https://fairys-wand.herokuapp.com/venue">
-                    <img src="https://i.imgur.com/1hzOSZQl.png" alt="" />
-                </a>
-
+                    <a href="https://fairys-wand.herokuapp.com/venue">
+                        <img src="https://i.imgur.com/1hzOSZQl.png" alt="" />
+                    </a>
+                    <a href="https://fairys-wand.herokuapp.com/venue">
+                        <img src="https://i.imgur.com/1hzOSZQl.png" alt="" />
+                    </a>
+                    <a href="https://fairys-wand.herokuapp.com/venue">
+                        <img src="https://i.imgur.com/1hzOSZQl.png" alt="" />
+                    </a>
+                </div>
             </div >
         )
     }
