9d5c257de6e39033c8bd231b8ae057b4184dcb82
ANNIEB45 <ANNIEBELLEFLEUR@GMAIL.com>
2020-07-27 22:44:02 -0400

    updated education timeline

--- portfolio/src/components/resume/education.jsx
+++ portfolio/src/components/resume/education.jsx
@@ -17,7 +17,7 @@ export default class education extends Component {
                                 <TimelineContent>Atlanta, GA </TimelineContent>
                                 <TimelineContent>March 2020-June 2020</TimelineContent>
                             </div>
-                            <TimelineContent>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </TimelineContent>
+                            <TimelineContent>Software Engineer Immersive <br /> 12-week, 500+ hour training program focused in Git/GitHub, HTML/CSS, JavaScript, MongoDB, SQL, Python, Django, React, Heroku, etc. </TimelineContent>
                         </div>
                     </TimelineItem>
 
@@ -32,7 +32,7 @@ export default class education extends Component {
                                 <TimelineContent>Atlanta, GA </TimelineContent>
                                 <TimelineContent>August 2012- September 2014</TimelineContent>
                             </div>
-                            <TimelineContent>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </TimelineContent>
+                            <TimelineContent>A.S. Respiratory Therapy <br /> Dean's List </TimelineContent>
                         </div>
                     </TimelineItem>
                 </Timeline>
