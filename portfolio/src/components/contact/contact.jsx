b924d66715f2712f61e3bfd16d103e20e4d17bd1
ANNIEB45 <ANNIEBELLEFLEUR@GMAIL.com>
2020-07-31 18:59:18 -0400

    updated form

--- portfolio/src/components/contact/contact.jsx
+++ portfolio/src/components/contact/contact.jsx
@@ -1,8 +1,7 @@
 import React, { Component } from 'react'
 import Social from './social'
+import emailjs from 'emailjs'
 import axios from 'axios'
-import { Form, Col, Button } from 'react-bootstrap'
-import emailjs from "emailjs-com"
 
 import './contact.css'
 
@@ -18,39 +17,32 @@ export default class contact extends Component {
         }
     }
 
-
-
-
-
     handeleOnSubmit = (evt) => {
         evt.preventDefault()
         console.log('i was clicked')
 
+        let template_params = {
+            "user_email": "user_email_value",
+            "first_name": "first_name_value",
+            "last_name": "last_name_value",
+            "company_name": "company_name_value",
+            "subject": "subject_value",
+            "message": "message_value",
+            "user_name": "user_name_value"
+        }
 
-        // let template_params = {
-        //     "user_email": "user_email_value",
-        //     "first_name": "first_name_value",
-        //     "last_name": "last_name_value",
-        //     "company_name": "company_name_value",
-        //     "subject": "subject_value",
-        //     "message": "message_value",
-        //     "user_name": "user_name_value"
-        //  }
+        const service_id = "default_service";
+        const template_id = "portfolio_email";
+        emailjs.send(service_id, template_id, template_params);
 
-        //  const service_id = "default_service";
-        //  const template_id = "portfolio_email";
-        //  emailjs.send(service_id, template_id, template_params);
-
-        // resetForm = () => {
-        //     this.setState({
-        //         firstName: '',
-        //         lastName: '',
-        // companyName: '',
-        //         email: '',
-        //         subject: '',
-        //         message: ''
-        //     })
-        // }
+            const newState = { ...this.state }
+            newState.newEmail.firstName = ''
+            newState.newEmail.lastName = ''
+            newState.newEmail.companyName =  ''
+            newState.newEmail.email = ''
+            newState.newEmail.subject = ''
+            newState.newEmail.message = ''
+    
 
     }
 
@@ -61,80 +53,79 @@ export default class contact extends Component {
         console.log(evt.target.value)
     }
 
+
     render() {
         const { firstName, lastName, companyName, subject, email, message } = this.state.newEmail
         return (
             <div className='contact'>
 
-                <Form onSubmit={ this.handeleOnSubmit}>
-                    <Form.Group controlId="formBasicName">
-                        <Form.Row>
-                            <Col>
-                                <Form.Control
-                                    type="text"
-                                    name="firstName"
-                                    value={ firstName }
-                                    onChange={ this.handleOnChange }
-                                    placeholder="First Name" />
-                            </Col>
-                            <Col>
-                                <Form.Control
-                                    type="text"
-                                    name="lastName"
-                                    value={ lastName }
-                                    onChange={ this.handleOnChange }
-                                    placeholder="Last Name" />
-                            </Col>
-                        </Form.Row>
-                    </Form.Group>
+                <form
+                    onSubmit={ this.handleOnSubmit }
+                    method='POST'>
 
-                    <Form.Group controlId="formBasicCompany">
-                        <Form.Label></Form.Label>
-                        <Form.Control
-                            type="text"
-                            name="companyNamne"
-                            value={ companyName }
-                            onChange={ this.handleOnChange }
-                            placeholder="Company Name" />
-                    </Form.Group>
+                    <label htmlFor='name'>Name</label>
+                    <input
+                        id='name'
+                        type='text'
+                        name='firstName'
+                        value={ firstName }
+                        placeholder='First Name'
+                        onChange={ this.handleOnChange }
+                    />
 
-                    <Form.Group controlId="formBasicEmail">
-                        <Form.Label></Form.Label>
-                        <Form.Control
-                            type="email"
-                            name="email"
-                            value={ email }
-                            onChange={ this.handleOnChange }
-                            placeholder="name@email.com" />
-                    </Form.Group>
+                    <label htmlFor='lastName'>Last Name</label>
+                    <input
+                        id='lastnName'
+                        type='text'
+                        name='lastName'
+                        value={ lastName }
+                        placeholder=' Last Name'
+                        onChange={ this.handleOnChange }
+                    />
 
-                    <Form.Group controlId="formBasicSubject">
-                        <Form.Label></Form.Label>
-                        <Form.Control
-                            type="text"
-                            name="subject"
-                            value={ subject }
-                            onChange={ this.handleOnChange }
-                            placeholder="Subject" />
-                    </Form.Group>
+                    <label htmlFor='companyName'>Company Name</label>
+                    <input
+                        id='companyName'
+                        type='text'
+                        name='companyName'
+                        value={ companyName }
+                        placeholder='Company Name'
+                        onChange={ this.handleOnChange }
+                    />
 
+                    <label htmlFor='InputEmail'>Email</label>
+                    <input
+                        id='Inputemail'
+                        type='email'
+                        name='email'
+                        placeholder='Enter Your Email'
+                        value={ email }
+                        onChange={ this.handleOnChange }
+                    />
 
-                    <Form.Group controlId="formBasicMessage">
-                        <Form.Label></Form.Label>
-                        <Form.Control
-                            as="textarea"
-                            rows="3"
-                            name="message"
-                            value={ message }
-                            onChange={ this.handleOnChange }
-                            placeholder="Message" />
-                    </Form.Group>
+                    <label htmlFor='subject'>Subject</label>
+                    <input
+                        id='subject'
+                        type='text'
+                        name='subject'
+                        value={ subject}
+                        placeholder='Subject'
+                        onChange={ this.handleOnChange }
+                    />
 
-                    <Button variant="primary" type="submit" >Submit</Button>
-                </Form>
+                    <label htmlFor='message'>Message</label>
+                    <textarea
+                        id='message'
+                        type='text'
+                        name='message'
+                        rows='5'
+                        value={ message }
+                        onChange={ this.handleOnChange }
+                    ></textarea>
 
+                    <input type="submit" value='Send' />
+                </form>
                 <Social />
-
                 <div className='images'>
                     {/* <img className="bottom-img" src="https://bit.ly/2OmNqdV" alt="" /> */ }
                     {/* <img className="top-img" src="" alt="" /> */ }
